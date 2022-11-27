959e1321f6ed286762c76d499306a85bb1897ab7
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2022-11-27 16:57:16 +0100

    naming

--- coroutines/src/main/kotlin/OwnContinuation.kt
+++ coroutines/src/main/kotlin/OwnContinuation.kt
@@ -3,20 +3,18 @@ import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.startCoroutine
 
-suspend fun susanswer() = 3
+suspend fun suspendAnswer() = 42
 
-suspend fun sussqr(x: Int) = x * x
-
-suspend fun sussum(x: Int, y: Int) = x + y
+suspend fun suspendSqr(x: Int) = x * x
 
 fun main() {
-    ::susanswer.startCoroutine(object : Continuation<Int> {
+    ::suspendAnswer.startCoroutine(object : Continuation<Int> {
         override val context: CoroutineContext
             get() = CoroutineName("Empty Context Simulation")
 
         override fun resumeWith(result: Result<Int>) {
             val prevResult = result.getOrThrow()
-            ::sussqr.startCoroutine(prevResult,
+            ::suspendSqr.startCoroutine(prevResult,
                 Continuation(
                     CoroutineName("")
                 ) { it: Result<Int> ->
